c62091a6e172a28c1399361f25c165353f7b3247
baerwang <1780285721@qq.com>
2023-01-21 18:45:46 +0800

    fix: dynamic change giscus background color

--- docs/giscus.js
+++ docs/giscus.js
@@ -1,58 +1,45 @@
-var giscus = function () {
+window.addEventListener('load', function () {
     document.getElementById("theme-list").addEventListener("click", function (e) {
-        var iframe = document.querySelector('.giscus-frame');
+        let iframe = document.querySelector('.giscus-frame');
         if (!iframe) return;
-        var theme;
-        if (e.target.className === "theme") {
-            theme = e.target.id;
-        } else {
-            return;
-        }
+
+        if (e.target.className !== "theme theme-selected") return;
+
+        let theme = e.target.id;
 
         // 若当前 mdbook 主题不是 Light 或 Rust ，则将 giscuz 主题设置为 transparent_dark
-        var giscusTheme = "light"
-        if (theme != "light" && theme != "rust") {
+        let giscusTheme = "light"
+        if (theme !== "light" && theme !== "rust") {
             giscusTheme = "transparent_dark";
         }
 
-        var msg = {
-            setConfig: {
-                theme: giscusTheme
-            }
-        };
-        iframe.contentWindow.postMessage({giscus: msg}, 'https://giscus.app');
+        iframe.contentWindow.postMessage({giscus: {setConfig: {theme: giscusTheme}}}, 'https://giscus.app');
     });
 
-    var giscusTheme = "light";
+    let giscusTheme = "light";
     const themeClass = document.getElementsByTagName("html")[0].className;
-    if (themeClass.indexOf("light") == -1 && themeClass.indexOf("rust") == -1) {
+    if (themeClass.indexOf("light") === -1 && themeClass.indexOf("rust") === -1) {
         giscusTheme = "transparent_dark";
     }
 
-    console.log(giscusTheme)
+    const element = document.createElement("script");
+    element.type = "text/javascript";
+    element.src = "https://giscus.app/client.js";
+    element.crossOrigin = "anonymous";
+    element.async = true;
 
-    var script = document.createElement("script");
-    script.type = "text/javascript";
-    script.src = "https://giscus.app/client.js";
-    script.crossOrigin = "anonymous";
-    script.async = true;
-
-    script.setAttribute("data-repo", "baerwang/mdbook");
-    script.setAttribute("data-repo-id", "R_kgDOIzBFsg");
-    script.setAttribute("data-category", "General");
-    script.setAttribute("data-category-id", "DIC_kwDOIzBFss4CTshs");
-    script.setAttribute("data-mapping", "pathname");
-    script.setAttribute("data-term", "0");
-    script.setAttribute("data-reactions-enabled", "1");
-    script.setAttribute("data-emit-metadata", "0");
-    script.setAttribute("data-input-position", "bottom");
-    script.setAttribute("data-theme", giscusTheme);
-    script.setAttribute("data-lang", "zh-CN");
-    script.setAttribute("data-loading", "lazy");
-
-
-    document.getElementById("giscus-container").appendChild(script);
-};
-
-window.addEventListener('load', giscus);
+    element.setAttribute("data-repo", "baerwang/mdbook");
+    element.setAttribute("data-repo-id", "R_kgDOIzBFsg");
+    element.setAttribute("data-category", "General");
+    element.setAttribute("data-category-id", "DIC_kwDOIzBFss4CTshs");
+    element.setAttribute("data-mapping", "pathname");
+    element.setAttribute("data-term", "0");
+    element.setAttribute("data-reactions-enabled", "1");
+    element.setAttribute("data-emit-metadata", "0");
+    element.setAttribute("data-input-position", "bottom");
+    element.setAttribute("data-theme", giscusTheme);
+    element.setAttribute("data-lang", "zh-CN");
+    element.setAttribute("data-loading", "lazy");
+    document.getElementById("giscus-container").appendChild(element);
+});
 
